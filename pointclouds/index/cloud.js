b57d912cb6b8c79c76aa6847e4bb8188704236c7
Antony Marcos Barja Ingaruca <antony.barja8@gmail.com>
2024-01-29 11:08:31 -0500

    LAS visualization

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=18 +south +datum=WGS84 +units=m +no_defs",
+    "points": 2217505,
+    "boundingBox": {
+        "lx": 870682.72,
+        "ly": 8502674.46,
+        "lz": 3352.46,
+        "ux": 872679.47,
+        "uy": 8504671.21,
+        "uz": 5349.21
+    },
+    "tightBoundingBox": {
+        "lx": 870682.72,
+        "ly": 8502674.46,
+        "lz": 3352.46,
+        "ux": 872189.85,
+        "uy": 8504671.21,
+        "uz": 3582.91
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 17.292362213134767,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
